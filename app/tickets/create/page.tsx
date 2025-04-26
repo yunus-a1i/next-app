c63697fbb4c4b539e9679a527bfbcaf256e43844
Yunus Ali Siddiqui <yunusalisiddiqui90@gmail.com>
2025-04-26 23:20:19 +0530

    Create page.tsx

---
+++ app/tickets/create/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CreateForm from './CreateForm'
+
+function CreateTickets() {
+  
+  return (
+    <main>
+      <h2 className='text-center'>Add a new Ticket</h2>
+      <CreateForm/>
+    </main>
+  )
+}
+
+export default CreateTickets
